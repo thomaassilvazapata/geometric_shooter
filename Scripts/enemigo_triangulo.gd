fb590711cf936155b332f0508c643ce50b97a73f
thomaassilvazapata <thomaass21@gmail.com>
2026-05-06 13:57:28 -0500

    disparo funcional y enemigos siguen el area

--- Scripts/enemigo_triangulo.gd
+++ Scripts/enemigo_triangulo.gd
@@ -3,24 +3,17 @@ extends CharacterBody2D
 var speed: float = 120
 #var detection_range: float = 500.0
 
-@onready var jugador = get_parent().find_child("Jugador")
+@onready var area = get_parent().find_child("AreaVulnerable")
 
 func _ready():
-	jugador = get_tree().get_first_node_in_group("Jugador")
+	area = get_tree().get_first_node_in_group("Jugador")
 
 func _physics_process(_delta):
-	if jugador == null:
-		print("no me puedo mover")
+	if area == null:
 		return
 	
 	# Calcular dirección hacia el jugador
-	var direction = global_position.direction_to(jugador.global_position)
-	
-	# Opcional: solo perseguir si está dentro del rango
-	# var distance = global_position.distance_to(player.global_position)
-	# if distance > detection_range:
-	#     return
-	
-	# Mover hacia el jugador
+	var direction = global_position.direction_to(area.global_position)
+
 	velocity = direction * speed
 	move_and_slide()
